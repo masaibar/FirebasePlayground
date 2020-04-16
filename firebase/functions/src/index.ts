508ef0a28b1d4028e387ac1e407569342833a62b
masaibar <masaibar.dev@gmail.com>
2020-04-16 19:13:52 +0900

    ファイルがアップロードされたら呼び出される関数を追加

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -4,5 +4,14 @@ import * as functions from 'firebase-functions';
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!!");
+    response.send("Hello from Firebase!!");
+});
+
+export const uploaded = functions.storage.object().onFinalize(async (object) => {
+    const fileBucket = object.bucket;
+    const filePath = object.name;
+    const contentType = object.contentType;
+    const metageneration = object.metageneration;
+
+    console.log("fileBucket: " + fileBucket + ", filePath: " + filePath + ", contentType: " + contentType + ", metageneration: " + metageneration)
 });
